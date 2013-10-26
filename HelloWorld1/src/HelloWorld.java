ebb673ac36a7ab9fe0dc8ea256b47860c502a2d6
mathu <mathura1987@gmail.com>
2013-10-25 20:44:06 -0700

    One more print message

--- HelloWorld1/src/HelloWorld.java
+++ HelloWorld1/src/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello World");
+		System.out.print("I am crazy");
 
 	}
 
