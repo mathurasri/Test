d31096c52108af7f51f7fda6723509bc39ac5e4f
mathu <mathura1987@gmail.com>
2014-01-04 08:06:12 -0500

    First Panda HelloWorld

---
+++ HelloWorld/HelloWorld/HelloWorld1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class HelloWorld1
+    {
+        static void Main(string[] args)
+        {
+            Panda panda1 = new Panda("Panda1");
+            Console.WriteLine("Panda1 Info:" + " " + panda1.name + " " + Panda.population);
+            
+            Panda panda2 = new Panda("Panda2");            
+            Console.WriteLine("Panda2 Info:" + " " + panda2.name + " " + Panda.population);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Panda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Panda
+    {
+        public string name;
+        public static int population;
+
+        public Panda(string name)
+        {
+            this.name = name;
+            population = population + 1;
+        }
+    }
+}
